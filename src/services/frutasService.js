7fe36e9b64381caf6563a5bdece4d82f19d38f93
laisavargas-blip <lais_a_vargas@estudante.sesisenai.org.br>
2026-09-15 16:54:49 -0300

    passo 2

---
+++ src/services/frutasService.js
@@ -0,0 +1,31 @@
+const pool = require("../database/connection");
+
+async function listarFrutas() {
+    const resultado = await pool.query("SELECT * FROM frutas");
+    return resultado.rows;
+}
+
+async function buscarFruta(id) {
+    const resultado = await pool.query(
+        "SELECT * FROM frutas WHERE id = $1",
+        [id]
+    );
+    return resultado.rows[0];
+}
+
+async function criarFruta(nome, tipo, preco, quantidade) {
+    const resultado = await pool.query(
+        `INSERT INTO frutas (nome, tipo, preco, quantidade)
+         VALUES ($1, $2, $3, $4)
+         RETURNING *`,
+        [nome, tipo, preco, quantidade]
+    );
+
+    return resultado.rows[0];
+}
+
+module.exports = {
+    listarFrutas,
+    buscarFruta,
+    criarFruta
+};
